3255ff87f50c1202886ec545cf08b7047a2a29d6
Andrés Muñoz <andres.munoz.b@usach.cl>
2019-11-29 17:55:01 -0300

    First commit
    Signed-off-by: Andrés Muñoz <andres.munoz.b@usach.cl>

---
+++ index.js
@@ -0,0 +1,39 @@
+'use strict'
+
+const express = require('express')
+const bodyParser = require("body-parser")
+const mongoose = require("mongoose")
+
+//SERVICES
+const ProductService = require('./services/product.service')
+
+const app = express()
+const port = process.env.PORT || 3000
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+// API PRODUCTS
+app.get('/api/products', (req, res) => { ProductService.getAll(req, res) })
+app.get('/api/products/:id', (req, res) => { ProductService.findById(req, res) })
+app.post('/api/products', (req, res) => { ProductService.create(req, res) })
+app.put('/api/products/:id', (req, res) => { ProductService.update(req, res) })
+app.delete('/api/products/:id', (req, res) => { ProductService.delete(req, res) })
+
+//CONNECTION TO DATABASE
+mongoose.connect(
+    'mongodb://localhost:27017',
+    {
+        useFindAndModify: false,
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        user: 'root',
+        pass: 'example',
+        dbName: 'shop'
+    },
+    err => {
+        if (err) return console.log(`Error al conectar a la base de datos ${err}`)
+        console.log('Conexión a la base de datos establecida')
+        app.listen(port, () => {console.log(`API REST running in http://localhost:${port}`)})
+    }
+)
+
---
+++ services/product.service.js
@@ -0,0 +1,44 @@
+//MODELS
+const Product = require('../models/product')
+
+const ProductService = {
+    getAll(req, res) {
+        Product.find({}, (err, products) => {
+            if (err) return res.status(500).send({ message: `Error al realizar petición: ${err}` })
+            res.status(200).send(products)
+        })
+    },
+    findById(req, res) {
+        let id = req.params.id
+        Product.findById(id, (err, product) => {
+            if (err) return res.status(500).send({ message: `Error al realizar petición: ${err}` })
+            if (!product) return res.status(400).send({ message: `El producto no existe` })
+            res.status(200).send({ product: product })
+        })
+    },
+    create(req, res) {
+
+        let product = new Product(req.body);
+        product.save((err, productStored) => {
+            if (err) res.status(500).send({ message: `Error al guardar en la base de datos: ${err}` })
+            res.status(201).send({ product: productStored })
+        })
+    },
+    delete(req, res) {
+        let id = req.params.id
+        Product.findByIdAndDelete(id, (err) => {
+            if (err) return res.status(500).send({ message: `Error al realizar petición: ${err}` })
+            res.status(200).send({ message: `El producto se ha eliminado` })
+        })
+    },
+    update(req, res) {
+        let id = req.params.id
+        let body = req.body
+        Product.findByIdAndUpdate(id, body, (err, productUpdated) => {
+            if (err) return res.status(500).send({ message: `Error al actualizar el productor : ${err}` })
+            res.status(200).send({ product: productUpdated })
+        })
+    }
+}
+
+module.exports = ProductService;
